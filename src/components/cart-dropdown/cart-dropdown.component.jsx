3d4e37159a7dd0f46989eb8beb88654bd99b1a4a
Tutul101 <60464726+Tutul101@users.noreply.github.com>
2023-11-26 16:04:17 +0530

    transform cart dropdown and cart icon component into styled component

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -5,7 +5,11 @@ import { CartContext } from "../../contexts/cart.context";
 import Button from "../button/button.component";
 import CartItem from "../cart-item/cart-item.component";
 
-import "./cart-dropdown.styles.scss";
+import {
+  CartDropDownContainer,
+  CartItems,
+  EmptyMessage,
+} from "./cart-dropdown.styles.jsx";
 
 const CartDropDown = () => {
   const { cartItems } = useContext(CartContext);
@@ -16,14 +20,21 @@ const CartDropDown = () => {
     navigate("/checkout");
   };
   return (
-    <div className="cart-dropdown-container">
-      <div className="cart-items">
-        {cartItems.map((item) => {
-          return <CartItem key={item.id} cartItem={item} />;
-        })}
-      </div>
+    <CartDropDownContainer>
+      <CartItems>
+        {cartItems.length ? (
+          <>
+            {cartItems.map((item) => {
+              return <CartItem key={item.id} cartItem={item} />;
+            })}
+          </>
+        ) : (
+          <EmptyMessage> Your cart is empty</EmptyMessage>
+        )}
+      </CartItems>
+
       <Button onClick={checkoutHandler}>CHECKOUT</Button>
-    </div>
+    </CartDropDownContainer>
   );
 };
 
--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -2,7 +2,11 @@ import React, { useContext } from "react";
 import { CartContext } from "../../contexts/cart.context";
 import { ReactComponent as ShoppingIcon } from "../../assets/shopoing-bag.svg";
 
-import "./cart-icon.styles.scss";
+import {
+  CartIconContainer,
+  ShopingIcon,
+  ItemCount,
+} from "./cart-icon.styles.jsx";
 
 const CartIcon = () => {
   const { isCartOpen, setIsCartOpen, cartCount } = useContext(CartContext);
@@ -11,10 +15,11 @@ const CartIcon = () => {
     setIsCartOpen(!isCartOpen);
   };
   return (
-    <div className="cart-icon-container" onClick={toggleIsCartOpen}>
-      <ShoppingIcon className="shopping-icon" />
-      <span className="item-count">{cartCount}</span>
-    </div>
+    <CartIconContainer onClick={toggleIsCartOpen}>
+      <ShopingIcon />
+
+      <ItemCount>{cartCount}</ItemCount>
+    </CartIconContainer>
   );
 };
 
